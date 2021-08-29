8ad96204b2766a906a1152281a4303c296900d74
NikkiMounce <nikkimounce@gmail.com>
2021-08-29 15:47:34 -0700

    finished engineer class

--- script/classes/engineer.js
+++ script/classes/engineer.js
@@ -1,16 +1,21 @@
 const employee = require('./employee');
+const writeHTML = require('./script/writeHTML.js')
 
 class engineer extends employee {
-    constructor(id, Github) {
-        super(name, id, email, 'Engineer');
-        this.GitHub = Github;
+    constructor(name, id, email, github) {
+        super(name, id, email);
+        this.gitHub = github;
+    }
+
+    getRole() {
+        return "Engineer"
+    }
+    getGithub() {
+        return this.github;
+    }
+    getHTML() {
+        return writeHTML.engineer(this);
     }
 }
-engineer.printInfo();
 
-
-* `github`&mdash;GitHub username
-
-* `getGithub()`
-
-* `getRole()`&mdash;overridden to return `'Engineer'`
+module.exports = engineer;
