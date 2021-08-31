20bfc00f4766fa2e827e0839a186a470632aa9a7
NikkiMounce <nikkimounce@gmail.com>
2021-08-31 08:40:12 -0700

    finished new app.js file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 https://github.com/TheBrellis/Template-Engine-Employee-Summary/blob/master
+https://github.com/JeffQuit/OOP---Team-Profile-Generator/blob/master/app.js
--- script/app2.js
+++ script/app2.js
@@ -7,6 +7,7 @@ const fs = require('fs');
 const OUTPUT_DIR = path.resolve(__dirname, 'output');
 const outputPath = path.join(OUTPUT_DIR, 'team.html');
 const render = require('../script/HTML template/writeHTML');
+const writeHTML = require('../script/HTML template/writeHTML');
 
 const log = new Logger();
 
@@ -28,7 +29,7 @@ const managerQ = [{
         message: 'We will begin with the Manager. What is the Managers first name?',
     },
     {
-        name: 'mangerId',
+        name: 'managerId',
         type: 'input',
         message: `What is the Managers' employee ID number?`,
     },
@@ -43,3 +44,132 @@ const managerQ = [{
         message: `What is the Managers' office number?`
     }
 ];
+
+//Question for adding another team member
+const addAnotherTM = {
+    name: 'add',
+    type: 'confirm',
+    message: 'Would you like to add another team member?',
+};
+
+//Question to determine which type of new TM to add
+const addAnotherTMRole = {
+    name: 'role',
+    type: 'list',
+    message: 'Is the team member an Engineer or an Intern?',
+    choices: ['Engineer', 'Intern'],
+};
+
+//Engineer specific questions
+const engineerQ = [{
+        name: 'engineerName',
+        type: 'input',
+        message: `What is the Engineers' name?`
+    },
+    {
+        name: 'engineerId',
+        type: 'input',
+        message: `What is the Engineers' employee ID number?`,
+    },
+    {
+        name: 'engineerEmail',
+        type: 'input',
+        message: `What is the Engineers' E-Mail address?`
+    },
+    {
+        name: 'engineerGithub',
+        type: 'input',
+        message: `What is the Engineers' GitHub profile name?`
+    },
+];
+
+//Intern specific questions
+const internQ = [{
+        name: 'internName',
+        type: 'input',
+        message: `What is the Interns' name?`
+    },
+    {
+        name: 'internId',
+        type: 'input',
+        message: `What is the Interns' employee ID number?`,
+    },
+    {
+        name: 'internEmail',
+        type: 'input',
+        message: `What is the Interns' E-Mail address?`
+    },
+    {
+        name: 'internSchool',
+        type: 'input',
+        message: `What School or University is the Intern attending?`
+    },
+];
+
+//func that renders all input
+
+function introQues() {
+    inquirer.prompt(introQ).then((appStart) => {
+        if (appStart.intro === 'Yes') {
+            log.green('Lets begin with the Managers information');
+            managerInfo();
+        } else {
+            log.orange(`Application Closed`);
+        }
+    });
+}
+
+//func to call manager info and build team
+function managerInfo() {
+    inquirer.prompt(managerQ).then((buildManager) => {
+        let manager = new Manager(buildManager.managerName, buildManager.managerId, buildManager.managerEmail, buildManager.managerOffice);
+        teamMemberArray.push(manager);
+        addAnother();
+    });
+}
+
+//func to add another TM
+function addAnother() {
+    inquirer.prompt(addAnotherTM).then((addTo) => {
+        //yes adds another tm to array and recalls addTMLoop func.
+        if (addTo.add === 'Yes') {
+            addTMLoop();
+        }
+        if (addTo.add === 'No') {
+            //no renders file and closes app
+            writeHTML(teamMemberArray);
+        }
+    });
+}
+
+//func to choose type of TM
+function addTMLoop() {
+    inquirer.prompt(addAnotherTMRole).then((TMRole) => {
+        if (TMRole.role === 'Engineer') {
+            log.yellow('Enter the Engineers Information');
+            inquirer.prompt(engineerQ).then((buildEngineer) => {
+                let engineer = new Engineer(buildEngineer.engineerName, buildEngineer.engineerId, buildEngineer.engineerEmail, buildEngineer.engineerGithub)
+                teamMemberArray.push(engineer);
+                addAnother();
+            });
+        } else if (TMRole.role === 'Intern') {
+            log.blue('Enter the Interns Information');
+            inquirer.prompt(internQ).then((buildIntern) => {
+                let intern = new Engineer(buildIntern.internName, buildIntern.internId, buildIntern.internEmail, buildIntern.internSchool)
+                teamMemberArray.push(intern);
+                addAnother();
+            });
+        }
+    });
+}
+
+//func to write array info to HTML when all tm's have been enetered
+async function writeHTML(file) {
+    const htmlDoc = render(file);
+    await writeFileAsync(outputPath, htmlDoc).then(function () {
+        log.green(`Team Profile Completed`);
+    });
+}
+
+//call start application
+introQues();
